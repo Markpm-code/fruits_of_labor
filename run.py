e20a53f0699052c1ee04a5c333c2d4e7c793023a
Markpm-code <marlonseglos@yahoo.com>
2022-09-20 10:34:18 +0000

    Add username validation

--- run.py
+++ run.py
@@ -12,6 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('fruits_of_labor')
 
+
+while True:
+    username = input("Please enter your name to continue:")
+
+    if username.isalpha() and (len(username) >= 4 and len(username) <= 10):
+        print("Your username is: " + username)  
+        break
+    else: 
+        print("Please enter correct characters not less than 4\n")
+        print("Please enter characters not more than 10\n")
+        print("It must be alphabet only\n")
+        continue 
+
+
 orders = SHEET.worksheet("orders")
 
 data = orders.get_all_values()
